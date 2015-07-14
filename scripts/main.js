72f1288cd0775fd15a605c6ea612761fe67f09f7
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-07-14 13:07:17 -0400

    added basic functions to object; need to add the intertwined changecard/checkcorrect functionality

--- scripts/main.js
+++ scripts/main.js
@@ -14,58 +14,86 @@ var flashcard = {
      correct: null}],
   // flashcard.deckFocus, one day, will contain specially marked cards
   deckFocus: [],
+  // cur is the current count or index of the card array
+  cur: 0,
+  // els contains the elements on the page
+  els: {
+    displayFront: $(".flashcards-front-1"),
+    displayBack: $(".flashcards-back-1"),
+    displayIncor: $(".incorrect-category"),
+    displayCor: $(".correct-category"),
+    moveToRight: $(".move-right"),
+    moveToLeft: $(".move-left"),
+    flipCard: $(".flip"),
+    bigBody: $("body"),
+    mkNewCard: $(".new-card")},
+  // the primary function
+  listen: function() {
+
+    // Rendering the cur count of i in the array on page load
+    this.els.displayFront.html(flashcard.deck[flashcard.cur].front)
+    this.els.displayBack.html(flashcard.deck[flashcard.cur].back)
+
+    // flipping the card
+    function flipTheCard() {
+      flashcard.els.displayFront.toggleClass("flashcard-hidden")
+      flashcard.els.displayBack.toggleClass("flashcard-hidden")
+    }
+    this.els.flipCard.click( flipTheCard )
+    this.els.bigBody.keydown(function(e) {
+      if(e.keyCode == 38) {
+        console.log("pressed 38")
+        flipTheCard();
+      }
+    })
+    this.els.bigBody.keydown(function(e) {
+      if(e.keyCode == 40) {
+        flipTheCard();
+      }
+    })
+
+    // Defining the function to create a new card
+    function newCard () {
+      newfront = prompt("What do you want the front to say?");
+      newback = prompt("What do you want the back to say?");
+
+      flashcard.deck.push({front: newfront, back: newback, correct: null});
+      console.log(flashcard.deck);
+    }
+    this.els.mkNewCard.click( newCard );
+
+    // Defining the functions to increase/decrease i ; move card right/left
+    this.els.moveToRight.click(function moveToRight() {
+
+    })
+  }
 }
+flashcard.listen();
 
   console.log("ready!");
-
-// Setting the counter to first index
-
-  var i = 0;
-
-// Rendering the current count of i in the array on page load
-
-$(".flashcards-front-1").html(flashcard.deck[i].front)
-$(".flashcards-back-1").html(flashcard.deck[i].back)
 
 // Defining the functions to increase/decrease i ; move card right/left
 
   function moveRight() {
     trueOrFalse();
-    if (i >= (flashcard.deck.length - 1)) {
-      i = 0;
+    if (flashcard.cur >= (flashcard.deck.length - 1)) {
+      flashcard.cur = 0;
     } else {
-      i++;
+      flashcard.cur++;
     }
-    $(".flashcards-front-1").html(flashcard.deck[i].front);
-    $(".flashcards-back-1").html(flashcard.deck[i].back);
+    flashcard.els.displayFront.html(flashcard.deck[flashcard.cur].front);
+    flashcard.els.displayBack.html(flashcard.deck[flashcard.cur].back);
   }
 
   function moveLeft() {
     trueOrFalse();
-    if (i === 0) {
-      i = (flashcard.deck.length - 1);
+    if (flashcard.cur === 0) {
+      flashcard.cur = (flashcard.deck.length - 1);
     } else {
-      i--;
+      flashcard.cur--;
     }
-    $(".flashcards-front-1").html(flashcard.deck[i].front);
-    $(".flashcards-back-1").html(flashcard.deck[i].back);
-  }
-
-// Defining the function to handle flipping the card
-
-  function flipCard() {
-    $(".flashcards-front-1").toggleClass("flashcard-hidden")
-    $(".flashcards-back-1").toggleClass("flashcard-hidden")
-  }
-
-// Defining the function to create a new card
-
-  function newCard () {
-    newfront = prompt("What do you want the front to say?");
-    newback = prompt("What do you want the back to say?");
-
-    flashcard.deck.push({front: newfront, back: newback, correct: null});
-    console.log(deck);
+    flashcard.els.displayFront.html(flashcard.deck[flashcard.cur].front);
+    flashcard.els.displayBack.html(flashcard.deck[flashcard.cur].back);
   }
 
 // Defining the function to evaluate .correct settings
@@ -73,26 +101,26 @@ $(".flashcards-back-1").html(flashcard.deck[i].back)
   // set the true/false value of .correct
 
   function setIncorrect () {
-    if (flashcard.deck[i].correct === null) {
+    if (flashcard.deck[flashcard.cur].correct === null) {
       console.log("Clicked incorrect. Status was null. Switching to false")
-      flashcard.deck[i].correct = false;
-    } else if (flashcard.deck[i].correct === false) {
+      flashcard.deck[flashcard.cur].correct = false;
+    } else if (flashcard.deck[flashcard.cur].correct === false) {
       console.log("Clicked incorrect. Status was false. Switching to null")
-      flashcard.deck[i].correct = null;
+      flashcard.deck[flashcard.cur].correct = null;
       }
-      console.log("Status ends at: " + flashcard.deck[i].correct)
+      console.log("Status ends at: " + flashcard.deck[flashcard.cur].correct)
       trueOrFalse();
   }
 
   function setCorrect () {
-    if (flashcard.deck[i].correct === null) {
+    if (flashcard.deck[flashcard.cur].correct === null) {
       console.log("Clicked correct. Status was null. Switching to true")
-      flashcard.deck[i].correct = true;
-      } else if (flashcard.deck[i].correct === true) {
+      flashcard.deck[flashcard.cur].correct = true;
+      } else if (flashcard.deck[flashcard.cur].correct === true) {
       console.log("Clicked correct. Status was true. Switching to null")
-      flashcard.deck[i].correct = null;
+      flashcard.deck[flashcard.cur].correct = null;
       }
-      console.log("Status ends at: " + flashcard.deck[i].correct)
+      console.log("Status ends at: " + flashcard.deck[flashcard.cur].correct)
       trueOrFalse();
   }
 
@@ -101,44 +129,42 @@ $(".flashcards-back-1").html(flashcard.deck[i].back)
   function trueOrFalse() {
     console.log("youre running trueOrFalse")
 
-    if ((flashcard.deck[i].correct === null) && ($(".incorrect-category").hasClass("answer-null"))) {
+    if ((flashcard.deck[flashcard.cur].correct === null) && (flashcard.els.displayIncor.hasClass("answer-null"))) {
       console.log("null and we're good to go! I'm not doing shit");
 
-    } else if ((flashcard.deck[i].correct === null) && ($(".incorrect-category").hasClass("answer-false")))  {
+    } else if ((flashcard.deck[flashcard.cur].correct === null) && (flashcard.els.displayIncor.hasClass("answer-false")))  {
       console.log("if condition met");
-      $(".incorrect-category").toggleClass("answer-false");
-      $(".incorrect-category").toggleClass("answer-null");
+      flashcard.els.displayIncor.toggleClass("answer-false");
+      flashcard.els.displayIncor.toggleClass("answer-null");
 
-    } else if ((flashcard.deck[i].correct === null) && ($(".incorrect-category").hasClass("answer-true")))  {
+    } else if ((flashcard.deck[flashcard.cur].correct === null) && (flashcard.els.displayIncor.hasClass("answer-true")))  {
         console.log("if condition met");
-        $(".incorrect-category").toggleClass("answer-true");
-        $(".incorrect-category").toggleClass("answer-null");
+        flashcard.els.displayIncor.toggleClass("answer-true");
+        flashcard.els.displayIncor.toggleClass("answer-null");
 
 // need a "this" statement for this...
 // otherwise I'd have to segment this massive function across both incorrect and correct.
 
-    } else if (flashcard.deck[i].correct === false) {
+    } else if (flashcard.deck[flashcard.cur].correct === false) {
       console.log("else if condition met")
-      $(".incorrect-category").toggleClass("answer-false");
-      $(".incorrect-category").toggleClass("answer-true");
+      flashcard.els.displayIncor.toggleClass("answer-false");
+      flashcard.els.displayIncor.toggleClass("answer-true");
 
-    } else if (flashcard.deck[i].correct === true) {
+    } else if (flashcard.deck[flashcard.cur].correct === true) {
       console.log("else if condition 2 met")
-      $(".correct-category").toggleClass("answer-false");
-      $(".correct-category").toggleClass("answer-true");
+      flashcard.els.displayCor.toggleClass("answer-false");
+      flashcard.els.displayCor.toggleClass("answer-true");
     }
   }
 
 // Defining listeners to trigger the functions
 
   // on click
-  $(".move-right").click( moveRight );
-  $(".move-left").click( moveLeft );
-  $(".flip").click( flipCard );
+  flashcard.els.moveToRight.click( moveRight );
+  flashcard.els.moveToLeft.click( moveLeft );
 
-  $(".incorrect-category").click( setIncorrect );
-  $(".correct-category").click( setCorrect );
-  $(".new-card").click( newCard );
+  flashcard.els.displayIncor.click( setIncorrect );
+  flashcard.els.displayCor.click( setCorrect );
 
   // on keypress
   $("body").keydown(function(e) {
@@ -150,18 +176,6 @@ $(".flashcards-back-1").html(flashcard.deck[i].back)
   $("body").keydown(function(e) {
     if(e.keyCode == 39) {
       moveRight();
-    }
-  })
-
-  $("body").keydown(function(e) {
-    if(e.keyCode == 38) {
-      flipCard();
-    }
-  })
-
-  $("body").keydown(function(e) {
-    if(e.keyCode == 40) {
-      flipCard();
     }
   })
 
